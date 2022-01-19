fcdc9e517b8337273abf3083f6c77bfcce971727
clyde0813 <eric3285@naver.com>
2022-01-19 13:32:56 +0900

    exception 빼고 완성

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+import json
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+class ShoppingDetail:
+    def __init__(self):
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument("headless")
+        self.options.add_argument(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+        self.wait = WebDriverWait(self.driver, 30)
+        self.target = json.load(open("target.json"))
+        self.conf = json.load(open("conf.json"))
+
+    def detail(self):
+        driver = self.driver
+        wait = self.wait
+        conf = self.conf
+        target = self.target
+        count = 1
+        d = {}
+        for i in target:
+            mall = target[i]["target"]
+            url = conf[mall]["url"] + target[i]["product_id"]
+            driver.get(url)
+            product_name = wait.until(EC.presence_of_element_located((By.XPATH, conf[mall]["productName"]))).text
+            origin_price = driver.find_element(By.XPATH, conf[mall]["originPrice"]).text
+            if conf[mall]["salePrice"] == "":
+                sale_price = None
+            else:
+                sale_price = driver.find_element(By.XPATH, conf[mall]["salePrice"]).text
+            if conf[mall]["discountPercent"] == "":
+                discount_percent = None
+            else:
+                discount_percent = driver.find_element(By.XPATH, conf[mall]["discountPercent"]).text
+            d[count] = {"target": mall, "url": url, "productName": product_name, "originPrice": origin_price,
+                        "salePrice": sale_price, "discountPercent": discount_percent}
+
+            count += 1
+            print(mall, " : ", product_name)
+
+        json.dump(d, open("output.json", "w"), ensure_ascii=False)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+shop = ShoppingDetail()
+shop.detail()
